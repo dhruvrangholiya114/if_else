71cccde437872ece35f2905459c7f7baa49d1385
dhruvrangholiya114 <130462930+dhruvrangholiya114@users.noreply.github.com>
2023-04-14 17:40:30 +0530

    Add files via upload

---
+++ if_else1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a=86,b=79;
+	if(a<b)
+	{
+		printf("%d is minimum number",a);
+	}
+	else
+	{
+		printf("%d is minimum number",b);
+	}
+}
---
+++ if_else2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a=7;
+	if (a==0)
+	{
+		printf("Given number %d is Neutral",a);
+	}
+	
+	else if (a<0)
+	{
+		printf("Given number %d is Negative",a);
+	}
+	else if (a>0)
+	{
+		printf("Given number %d is Positive",a);
+	}
+}
